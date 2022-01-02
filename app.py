35dd3415582fea8a0c2fe535ef9e10b46dd126b1
aakzsh <aakashferrari@gmail.com>
2022-01-02 21:40:04 +0530

    get results working

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+from geopy.geocoders import Nominatim
+import pandas as pd
+import geocoder
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -12,9 +15,20 @@ def index():
 def start():
     return render_template('start.html')
 
-@app.route('/results')
-def results():
-    return render_template('results.html')
+@app.route('/results/<lat>,<lng>')
+def results(lat, lng):
+    df = pd.read_csv('static/404.csv')
+    a = len(df[(float(lat)-0.027 <= df['lat']) & (df['lat'] <= float(lat)+0.027) & (float(lng)-0.025 <= df['lon']) & (df['lon'] <= float(lng)+0.025)])
+    send="MEDIUM"
+    clr="#5E17EB"
+    if a>500:
+        send="HIGH"
+        clr="red"
+    elif a<200:
+        send="LOW"
+        clr="green"
+    
+    return render_template('results.html', num=str(a), send=send, clr=clr, lmao = [lat,lng])
 
 @app.route('/measures')
 def measures():
@@ -24,5 +38,25 @@ def measures():
 def about():
     return render_template('about.html')
 
+@app.route('/geolol')
+def geolol():
+    loc = Nominatim(user_agent="GetLoc")
+    getLoc = loc.geocode("Andheri Mumbai")
+    print(getLoc.address)
+    print("Latitude = ", getLoc.latitude, "\n")
+    print("Longitude = ", getLoc.longitude)
+    return render_template('geo.html', lat=getLoc.latitude,lng= getLoc.longitude )
+
+@app.route('/curloc')
+def curloc():
+    g = geocoder.ip('me')
+    print(g.latlng)
+    return render_template('geo.html', lat=g.latlng[0],lng= g.latlng[1] )
+
+@app.route('/read')
+def read():
+    df = pd.read_csv('static/404.csv')
+    print(len(df[(70 <= df['lon']) & (df['lon'] <= 100)]))
+    return render_template('geo.html')
 if __name__ == "__main__":
     app.run(debug=True)
